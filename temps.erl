204773c11186b16155af9dd5d0c05e8492b2fe28
Jannis Andrija Schnitzer <Xjs@itisme.org>
2009-02-26 21:38:01 +0100

    added teh printing function again

--- temps.erl
+++ temps.erl
@@ -2,7 +2,8 @@
 -export([format_temps/1]).
 
 format_temps(List_of_cities) ->
-	convert_list_to_c(List_of_cities).
+	Converted_List = convert_list_to_c(List_of_cities),
+	print_temp(Converted_List).
 
 convert_list_to_c([{Name, {f, F}} | Rest]) ->
 	Converted_City = {Name, {c, (F -32) * 5 / 9}},
@@ -14,3 +15,8 @@ convert_list_to_c([City | Rest]) ->
 convert_list_to_c([]) ->
 	[].
 
+print_temp([{Name, {c, Temp}} | Rest]) ->
+	io:format("~-15w ~w c~n", [Name, Temp]),
+	print_temp(Rest);
+print_temp([]) ->
+	ok.
