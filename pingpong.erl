544009ae8ba044671a21e1f90e9738e3ffbafd38
Jannis Andrija Schnitzer <Xjs@itisme.org>
2009-03-01 15:24:32 +0100

    pingpong program

---
+++ pingpong.erl
@@ -0,0 +1,28 @@
+-module(pingpong).
+
+ping(0, Pong_Node) ->
+	{pong, Pong_Node} ! finished
+	io:format("ping finished~n", []);
+
+ping(N, Pong_Node) ->
+	{pong, Pong_Node) ! {ping, self()},
+	receive
+		pong ->
+			io:format("Ping received pong~n", [])
+	end,
+	ping(N - 1, Pong_Node).
+
+pong() ->
+	receive
+		finished ->
+			io:format("Pong finished~n", []);
+		{ping, Ping_PID} ->
+			io:format("Pong received ping~n", []),
+			Ping_PID ! pong,
+			pong()
+	end.
+
+start(Ping_Node) ->
+	register(pong, spawn(pingpong, pong, [])),
+	spawn(Ping_Node, pingpong, ping, [3, node()]).
+	
